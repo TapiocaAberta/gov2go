28a704d026fd15d9131f047cb040df59b99cdce3
pedro-hos <pedro-hos@outlook.com>
2014-09-17 21:37:11 -0300

    #9

--- gov2go-api/src/main/java/org/transparencia/gov2go/endpoints/util/ConfigureCORSFilter.java
+++ gov2go-api/src/main/java/org/transparencia/gov2go/endpoints/util/ConfigureCORSFilter.java
@@ -13,10 +13,6 @@ public class ConfigureCORSFilter implements ContainerResponseFilter {
 	@Override
 	public void filter(ContainerRequestContext requestContext,ContainerResponseContext responseContext) throws IOException {
 		  responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-	      responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-	      responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-	      responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-	      responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
 	}
 
 }
